f765ed378b06b686c0b26b4fa346b95336473bfc
ast2481 <universaltrendspaint@gmail.com>
2026-03-11 12:08:56 -0400

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,24 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+ 
+	int tankSize = 20;
+	double Townmpg = 23.5;
+	double Highwaympg = 28.9;
+
+	double distanceTown = tankSize * Townmpg;
+	double distanceHighway = tankSize * Highwaympg;
+
+	cout << "Distance"
+	
+	
+	
+	
+	
+	std::cout 
+}
+
